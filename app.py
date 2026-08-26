fc9e8c464b7f280cc95972a83dc00cdab40cf5da
bysanibalaji10-oss <bysanibalaji10@gmail.com>
2026-08-26 13:13:59 +0530

    Fix Vercel SQLite path

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db=SQLAlchemy(app)
 
 class Todo(db.Model):
